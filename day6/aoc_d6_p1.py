ce641057198e3534bcd1a19e12360d816a4c4f0f
Andrew Repp <71296468+ReppCodes@users.noreply.github.com>
2022-12-06 06:29:09 -0600

    Day 6 done.  Sets and max-heaps

---
+++ day6/aoc_d6_p1.py
@@ -0,0 +1,23 @@
+import os
+import heapq
+
+input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input_p1.txt")
+
+with open(input_path, "r") as fp:
+    stream = fp.read()
+
+trailingVals = []
+marker = None
+for idx, char in enumerate(stream):
+    if len(trailingVals) < 4:
+        heapq.heappush(trailingVals, (idx, char))
+    else:
+        heapq.heapreplace(trailingVals, (idx, char))
+
+    values = set([x[1] for x in trailingVals])
+    if len(values) == 4:
+        marker = idx+1
+        break
+
+print(trailingVals)
+print(f"The data stream begins at: {marker}")
